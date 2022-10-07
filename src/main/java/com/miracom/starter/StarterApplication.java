283bf585130efc9893e00450cb344a1f3d44e87e
Purst <holydays86@naver.com>
2022-10-07 23:23:53 +0900

    WAR 세팅 및 생성

--- src/main/java/com/miracom/starter/StarterApplication.java
+++ src/main/java/com/miracom/starter/StarterApplication.java
@@ -2,9 +2,16 @@ package com.miracom.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StarterApplication {
+public class StarterApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StarterApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterApplication.class, args);
